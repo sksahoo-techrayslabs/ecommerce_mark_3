9d9d572db82cc3831685e3b292864c2be5b3b275
sksahoo-techrayslabs <sksahoo@techrayslabs.com>
2026-03-05 17:35:50 +0530

    added customer_navbar.html and ts for it

---
+++ frontend/src/components/customer_navbar_loader.ts
@@ -0,0 +1,12 @@
+fetch("/frontend/src/components/customer_navbar.html")
+    .then((response: Response) => response.text())
+    .then((data: string) => {
+        const navbar = document.getElementById("customernavbar") as HTMLElement | null;
+
+        if (navbar) {
+            navbar.innerHTML = data;
+        }
+    })
+    .catch((error: Error) => {
+        console.error("Error loading admin navbar:", error);
+    });
