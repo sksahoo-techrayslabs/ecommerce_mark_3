9a07bcc25bb58fe6715f3f76f02eb4f6c7378f99
sksahoo-techrayslabs <sksahoo@techrayslabs.com>
2026-03-16 13:42:11 +0530

    implemented hashing in admin_login.ts

--- frontend/src/scripts/admin_login.ts
+++ frontend/src/scripts/admin_login.ts
@@ -1,31 +1,40 @@
+interface Admin {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+}
+
+import { hashPassword } from "../../dist/utils/password_hashing.js";
+
 const form = document.querySelector('form') as HTMLFormElement;
 
-form.addEventListener("submit", function (e: Event) {
+form.addEventListener("submit", async function (e: Event) {
     e.preventDefault();
 
     const email = (document.getElementById("email") as HTMLInputElement).value.trim();
     const password = (document.getElementById("password") as HTMLInputElement).value.trim();
+    const encoded_password = await hashPassword(password);
 
     // hardcoding admin details
     const admin_email: string = "admin@gmail.com";
     const admin_password: string = "1234567890";
+    //  password hashing
+    const hashedPassword = await hashPassword(admin_password);
 
-    if (email !== admin_email || password !== admin_password) {
+
+    if (email !== admin_email || encoded_password !== hashedPassword) {
         alert("Admin crededentials are wrong !!");
         return;
     }
 
-    interface Admin {
-        id: number;
-        name: string;
-        email: string;
-        role: string;
-    }
 
     const admin: Admin = {
         id: 1,
         name: "admin1",
         email: admin_email,
+        password: hashedPassword,
         role: "admin"
     };
 
