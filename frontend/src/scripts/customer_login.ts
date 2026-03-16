502367bb5ad525e03a25718a0f71d9168b57665b
sksahoo-techrayslabs <sksahoo@techrayslabs.com>
2026-03-16 12:52:13 +0530

    implemented hashing in customer login.ts

--- frontend/src/scripts/customer_login.ts
+++ frontend/src/scripts/customer_login.ts
@@ -1,64 +1,69 @@
 interface User {
-id: string;
-name: string;
-email: string;
-password: string;
-role: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
 }
+
+import { hashPassword } from "../../dist/utils/password_hashing.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-const loginForm = document.getElementById("customerloginform") as HTMLFormElement | null;
-const emailInput = document.getElementById("email") as HTMLInputElement | null;
-const passwordInput = document.getElementById("password") as HTMLInputElement | null;
+  const loginForm = document.getElementById("customerloginform") as HTMLFormElement | null;
+  const emailInput = document.getElementById("email") as HTMLInputElement | null;
+  const passwordInput = document.getElementById("password") as HTMLInputElement | null;
 
-if (!loginForm || !emailInput || !passwordInput) return;
-
-
-
-loginForm.addEventListener("submit", (e: Event) => {
-e.preventDefault();
-
-const email = emailInput.value.trim();
-const password = passwordInput.value.trim();
-
-// Validation
-if (!email || !password) {
-  alert("Email and Password are required");
-  return;
-}
-
-const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
-if (!emailPattern.test(email)) {
-  alert("Please enter a valid email");
-  return;
-}
-
-if (password.length < 6) {
-  alert("Password must be at least 6 characters");
-  return;
-}
-
-const users: User[] = JSON.parse(localStorage.getItem("users") || "[]");
-
-const customer = users.find((user: User) =>
-  user.email === email &&
-  user.password === password &&
-  user.role === "customer"
-);
-
-if (!customer) {
-  alert("Invalid customer email or password");
-  return;
-}
+  if (!loginForm || !emailInput || !passwordInput) return;
 
 
 
-localStorage.setItem("currentUser", JSON.stringify(customer));
+  loginForm.addEventListener("submit",async (e: Event) => {
+    e.preventDefault();
 
-window.location.href = "customer_products.html";
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
 
-});
+    // Validation
+    if (!email || !password) {
+      alert("Email and Password are required");
+      return;
+    }
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (!emailPattern.test(email)) {
+      alert("Please enter a valid email");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters");
+      return;
+    }
+    const hashedPassword = await hashPassword(password);
+
+    const users: User[] = JSON.parse(localStorage.getItem("users") || "[]");
+
+   
+
+    const customer = users.find((user: User) =>
+      user.email === email &&
+      user.password === hashedPassword &&
+      user.role === "customer"
+    );
+
+    if (!customer) {
+      alert("Invalid customer email or password");
+      return;
+    }
+
+
+
+    localStorage.setItem("currentUser", JSON.stringify(customer));
+
+    window.location.href = "customer_products.html";
+
+  });
 
 });
